ebfe9360fe6b919186ec240ec72c3c58671cc08f
mukur.pur <mukur.puri@gmail.com>
2025-03-25 15:28:04 +0530

    Merge

---
+++ SimulatedIntelligence/src/app/challenge-review/challenge-review.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge-review',
+  templateUrl: './challenge-review.component.html',
+  styleUrls: ['./challenge-review.component.scss']
+})
+export class ChallengeReviewComponent {
+
+}
