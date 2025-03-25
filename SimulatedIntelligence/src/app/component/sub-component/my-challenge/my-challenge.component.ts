eb0e8bfc9067e34843214095c6e01fe5a8e5c367
Mangesh Khose <mangesh.khose@rethinkfirst.com>
2025-03-25 15:45:47 +0530

    fetching my challenges from api with mock data done

--- SimulatedIntelligence/src/app/component/sub-component/my-challenge/my-challenge.component.ts
+++ SimulatedIntelligence/src/app/component/sub-component/my-challenge/my-challenge.component.ts
@@ -1,26 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AiAnalysisComponent } from '../view-ai-analysis/view-ai-analysis.component';
+import { DashboardService } from '../../dashboard-services/dashboard.service';
 
 @Component({
   selector: 'app-my-challenge',
   templateUrl: './my-challenge.component.html',
   styleUrls: ['./my-challenge.component.scss']
 })
-export class MyChallengeComponent  {
-  constructor(private dialog: MatDialog) {}
-  challenges = [
-    { title: 'Trigonometry & Angles Mastery', subject: 'Maths', date: 'Yesterday', grade: 'A+' },
-    { title: 'Advanced Calculus Challenge', subject: 'Maths', date: 'Yesterday', grade: 'A' },
-    { title: 'Number Systems & Arithmetic Reasoning', subject: 'Maths', date: '2 Days ago', grade: 'B+' },
-    { title: 'Statistics & Probability Assessment', subject: 'Maths', date: '3 Days ago', grade: 'C+' },
-    { title: 'Newton’s Laws & Motion Principles', subject: 'Physics', date: 'A week ago', grade: 'F' },
-    { title: 'Periodic Table & Chemical Reactions', subject: 'Chemistry', date: 'A week ago', grade: 'A' },
-    { title: 'Human Anatomy & Biological Systems', subject: 'Bio', date: 'A week ago', grade: 'B' },
-    { title: 'Electromagnetism', subject: 'Physics', date: 'Last Month', grade: 'C' },
-    { title: 'Earth & Space Science Exploration', subject: 'Physics', date: 'Last Month', grade: 'D+' },
-    { title: 'Pagination works', subject: 'Maths', date: 'Yesterday', grade: 'A+' },
-  ];
+export class MyChallengeComponent implements OnInit {
+  allChallenges: any[] = [];
+  constructor(private dialog: MatDialog,
+    private dashboardService: DashboardService
+  ) {}
+  ngOnInit(): void {
+    this.dashboardService.getAllChallenges().subscribe((data => { this.allChallenges = data }));
+  }
+
   // challenges object- title, subject, date, grade
   // getsubject- subject
   currentPage=1;
